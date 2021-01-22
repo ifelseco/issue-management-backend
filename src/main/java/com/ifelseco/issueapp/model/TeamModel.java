3c547376fa7755bf78b3b2738824f7163d8b8936
coderman <barslann58@gmail.com>
2021-01-21 21:55:24 -0800

    Team Entity created TeamModel created

---
+++ src/main/java/com/ifelseco/issueapp/model/TeamModel.java
@@ -0,0 +1,19 @@
+package com.ifelseco.issueapp.model;
+
+import com.ifelseco.issueapp.entity.Project;
+import com.ifelseco.issueapp.entity.User;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+import java.util.Set;
+
+
+@Data
+public class TeamModel {
+    private Long id;
+    private Set<User> members;
+    private Set<Project> projects;
+    private String name;
+    private LocalDateTime createTime;
+    private Long createdBy;
+}
