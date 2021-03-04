1a056b5afe7bd4cd45d92538fa678e00e2ffd596
coderman <barslann58@gmail.com>
2021-03-04 13:24:28 -0800

    project entity is implemented

--- src/main/java/com/ifelseco/issueapp/controller/ProjectController.java
+++ src/main/java/com/ifelseco/issueapp/controller/ProjectController.java
@@ -5,35 +5,48 @@ import com.ifelseco.issueapp.model.ProjectModel;
 import com.ifelseco.issueapp.model.TeamModel;
 import com.ifelseco.issueapp.service.ProjectService;
 import io.swagger.annotations.*;
+import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.security.Principal;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Set;
 
 @RestController
 @RequestMapping("/api/project")
 @Api("/api/project")
+@AllArgsConstructor
 public class ProjectController {
     private final ProjectService projectService;
 
-    public ProjectController(ProjectService projectService) {
-        this.projectService = projectService;
+    @PostMapping("/create/{selectedMemberIds}")
+    public ResponseEntity createProject(
+            @PathVariable List<Long> selectedMemberIds,
+            @Valid @RequestBody ProjectModel projectModel, Principal principal) {
+        return new ResponseEntity(projectService.create(selectedMemberIds, projectModel, principal), HttpStatus.CREATED);
     }
 
-    @PostMapping("/create")
-    @ApiOperation(value = "Create Project", notes = "Creating a new project", response = ProjectModel.class)
-    @ApiResponses({
-            @ApiResponse(code = 200, message = "Success", response = ProjectModel.class),
-            @ApiResponse(code = 400, message = "Bad request", response = ProjectModel.class)
-    })
-    public ResponseEntity createProject(
-            @ApiParam(required = true, name = "project", value = "New project")
-            @Valid @RequestBody ProjectModel projectModel, Principal principal) {
-        return new ResponseEntity(projectService.create(projectModel, principal), HttpStatus.CREATED);
+    @DeleteMapping("delete/{projectId}")
+    public void deleteTeam(@PathVariable Long projectId) {
+        projectService.deleteProject(projectId);
+    }
+
+    @GetMapping("{projectId}")
+    public ResponseEntity<TeamModel> getProjectById(@PathVariable("projectId") Long projectId) {
+        return new ResponseEntity(projectService.findProjectById(projectId), HttpStatus.OK);
+    }
+
+    @GetMapping()
+    public ResponseEntity<Set<ProjectModel>> getAllProjects() {
+        return new ResponseEntity(projectService.findAllProjects(), HttpStatus.OK);
+    }
+
+    @PutMapping("edit/{projectId}")
+    public ResponseEntity<TeamModel> editProject(@PathVariable("projectId") Long projectId, @RequestBody ProjectModel projectModel){
+        return new ResponseEntity(projectService.editProject(projectId, projectModel), HttpStatus.OK);
     }
 }
